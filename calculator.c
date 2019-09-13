4a6831b455bd509b9914cf89ac71460fbc1c8136
JeanPaulArmand <55025884+JeanPaulArmand@users.noreply.github.com>
2019-09-13 19:39:07 +0400

    Update calculator.c

--- calculator.c
+++ calculator.c
@@ -4,14 +4,17 @@ int main()
 {
     //menu bar
     int com;
-    int val1=0;
-    int val2=0;
+    int val1 = 0;
+    int val2 = 0;
     int res = 0;
+    int flag = 0;
 
     printf("Calculator prog v1.0 by Jean\n");
     while(1)
     {
         //printing menu
+        if(flag==0)
+        {
         printf("select from menu \n");
         printf("1 for add\n");
         printf("2 for mul\n");
@@ -19,6 +22,8 @@ int main()
         printf("4 for dev\n");
         printf("0 for exit\n");
         scanf("%d",&com);
+        flag=1;
+        }
         //basic logic of prog
         if ( com==1 )
         {
@@ -28,6 +33,7 @@ int main()
             scanf("$d",&val2);
             res=val1+val2;
             printf("\n res is %d",res);
+            flag=0;
 
         }
         else if(com==0) 
